be993bac6648c20b7e9076a2beef5dee2d135056
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-09 00:43:09 +0800

    feat: Updated the main.go to feature required routes

--- main.go
+++ main.go
@@ -1,8 +1,32 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/handlers"
+	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/services"
+)
+
+type RouteServices struct {
+	User services.User
+}
+
+func setupRouter(rs RouteServices) *gin.Engine {
+	router := gin.Default()
+	router.GET("/welcome", welcome)
+	router.GET("/user/:id", handlers.UserGet(rs.User))
+	return router
+}
 
 func main() {
-	router := gin.Default()
+	router := setupRouter()
 	router.Run(":8000")
+}
+
+func welcome(c *gin.Context) {
+	type Lol struct {
+		Status string
+	}
+	c.JSON(http.StatusOK, Lol{Status: "ok"})
 }
