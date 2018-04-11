5f6333caed43af20099c6cfcd0ac819c198891aa
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-12 00:47:19 +0800

    fix: Remove the welcome endpoint. At the same time, change the db to a more generalized name, storage. We can put in several connection strings into storage - redis connection

--- main.go
+++ main.go
@@ -1,32 +1,25 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/handlers"
 	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/services"
 )
 
 type RouteServices struct {
-	User services.User
+	Service services.Storage
+	User    services.User2
 }
 
 func setupRouter(rs RouteServices) *gin.Engine {
 	router := gin.Default()
-	router.GET("/welcome", welcome)
 	router.GET("/user/:id", handlers.UserGet(rs.User))
+	router.POST("/user", handlers.UserCreate(rs.User))
 	return router
 }
 
 func main() {
-	router := setupRouter()
+	rs := RouteServices{}
+	router := setupRouter(rs)
 	router.Run(":8000")
-}
-
-func welcome(c *gin.Context) {
-	type Lol struct {
-		Status string
-	}
-	c.JSON(http.StatusOK, Lol{Status: "ok"})
 }
