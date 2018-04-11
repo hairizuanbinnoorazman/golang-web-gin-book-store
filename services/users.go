1eaba644c1f9a8e2a2ae3cc062cf99cccc262dc2
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-12 00:50:38 +0800

    feat: Test out the current software architecture by attempting to implement a second service

--- services/users.go
+++ services/users.go
@@ -1,17 +1,39 @@
 package services
 
-import "github.com/hairizuanbinnoorazman/golang-web-gin-book-store/models"
+import (
+	"time"
+
+	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/models"
+)
 
 type User struct {
-	Service
+	Storage
 }
 
 func (u User) GetByID(ID string) models.User {
 	var user models.User
-	u.DB.Where("name = ?", "jinzhu").Find(&user)
+	// u.DB.Where("name = ?", "jinzhu").Find(&user)
+	user.FirstName = "LOL"
+	user.LastName = "MIAO"
 	return user
 }
 
-func (u User) Create(a *models.User) {
-	u.DB.Create(a)
+func (u User) Create(a *models.User) models.User {
+	// u.DB.Create(a)
+	lol := models.User{FirstName: "kcmkamcklamlc", LastLoginAt: time.Now()}
+	return lol
+}
+
+type User2 struct {
+	TestData models.User
+}
+
+func (u User2) GetByID(ID string) models.User {
+	u.TestData.FirstName = "lol"
+	u.TestData.LastName = "cacacarjnejkwkf"
+	return u.TestData
+}
+
+func (u User2) Create(a *models.User) models.User {
+	return *a
 }
