dba58114656284affdce835f52aed16243e05b52
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-14 02:40:39 +0800

    feat: Add initial set of tests which internal tests user model

---
+++ models/users_internal_test.go
@@ -0,0 +1,56 @@
+package models
+
+import (
+	"testing"
+)
+
+func TestValidateName(t *testing.T) {
+	type testCase struct {
+		TestName       string
+		InputFirstName string
+		InputLastName  string
+		ExpectedOutput string
+	}
+	cases := []testCase{
+		{TestName: "Empty String", InputFirstName: "", InputLastName: "", ExpectedOutput: "Name cannot be shorter than 8 characters"},
+	}
+
+	for _, singleCase := range cases {
+		tempUser := User{FirstName: singleCase.InputFirstName, LastName: singleCase.InputLastName}
+		err := tempUser.validateName()
+		if err != nil {
+			if err.Error() != singleCase.ExpectedOutput {
+				t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedOutput, err.Error())
+			}
+		} else {
+			if singleCase.ExpectedOutput != "nil" {
+				t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedOutput, "nil")
+			}
+		}
+	}
+}
+
+func TestValidatePassword(t *testing.T) {
+	type testCase struct {
+		TestName       string
+		Password       string
+		ExpectedOutput string
+	}
+	cases := []testCase{
+		{TestName: "Empty Password", Password: "", ExpectedOutput: "Password is too short"},
+	}
+
+	for _, singleCase := range cases {
+		tempUser := User{Password: singleCase.Password}
+		err := tempUser.validatePassword()
+		if err != nil {
+			if err.Error() != singleCase.ExpectedOutput {
+				t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedOutput, err.Error())
+			}
+		} else {
+			if singleCase.ExpectedOutput != "nil" {
+				t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedOutput, "nil")
+			}
+		}
+	}
+}
