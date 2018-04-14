6e32cf5191236c181bac2a5b162c96d5151d7d5b
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-14 19:11:29 +0800

    fix: Settings fileds as private will causes issues - need to set them back as public

--- models/users.go
+++ models/users.go
@@ -5,21 +5,22 @@ import (
 )
 
 // User struct defines the user entity in the application
+// Fields need to be set as public in order for other packages to access them
 type User struct {
-	ID                       string `json:"id"`
-	FirstName                string `json:"first_name"`
-	LastName                 string `json:"last_name"`
-	Email                    string `json:"email"`
-	Address                  string `json:"address"`
-	password                 string
-	forgetPasswordToken      string
-	forgetPasswordExpiryDate time.Time
-	activationToken          string
-	activationExpiryDate     time.Time
-	activated                bool
-	lastLoginAt              time.Time
-	createdAt                time.Time
-	updatedAt                time.Time
+	ID                       string    `json:"id"`
+	FirstName                string    `json:"first_name"`
+	LastName                 string    `json:"last_name"`
+	Email                    string    `json:"email"`
+	Address                  string    `json:"address"`
+	Password                 string    `json:"-"`
+	ForgetPasswordToken      string    `json:"-"`
+	ForgetPasswordExpiryDate time.Time `json:"-"`
+	ActivationToken          string    `json:"-"`
+	ActivationExpiryDate     time.Time `json:"-"`
+	Activated                bool      `json:"-"`
+	LastLoginAt              time.Time `json:"-"`
+	CreatedAt                time.Time `json:"-"`
+	UpdatedAt                time.Time `json:"-"`
 }
 
 type AdminUser struct {
@@ -33,7 +34,7 @@ type AdminUser struct {
 // NewUser returns a new user instance. During instantiation, several fields will need to be
 // instantiated; e.g. ID, Activated, CreatedAt, UpdatedAt, etc
 func NewUser(firstName, lastName, email, password string) *User {
-	return &User{FirstName: firstName, LastName: lastName, Email: email, password: password}
+	return &User{FirstName: firstName, LastName: lastName, Email: email, Password: password}
 }
 
 func (u User) validateName() error { return nil }
@@ -55,7 +56,10 @@ func (u *User) ForgetPassword() (string, error) { return "", nil }
 
 // ChangePasswordFromForget requires you to provide the forget password token. This function
 // will then check the forgetPasswordToken if its correct and alters it accordingly
-func (u *User) ChangePasswordFromForget(forgetPasswordToken string) error { return nil }
+func (u *User) ChangePasswordFromForget(forgetPasswordToken, password string) error { return nil }
+
+// ChangePassword changes the password on the user object before saving it
+func (u *User) ChangePassword(password string) error { return nil }
 
 // ReactivateToken resets the activation token in the case the user did not activate the account
 // in time. Returns an activationToken
@@ -64,6 +68,3 @@ func (u *User) ReactivateToken() (string, error) { return "", nil }
 // Activate user. You would need to provide a activation token to check if it correct.
 // If correct, it would return the status of the user which should be true or false
 func (u *User) Activate(activationToken string) (bool, error) { return false, nil }
-
-// IsActive checks if the user has already activated the token or not
-func (u *User) IsActive() bool { return false }
