ab9424bb184f3b04ab448f5cbd4e34a227d7b7dd
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-14 19:08:48 +0800

    feat: Add testing skeleton for testing address

--- models/users_internal_test.go
+++ models/users_internal_test.go
@@ -54,3 +54,28 @@ func TestValidatePassword(t *testing.T) {
 		}
 	}
 }
+
+func TestValidateAddress(t *testing.T) {
+	type testCase struct {
+		TestName       string
+		Address        string
+		ExpectedOutput string
+	}
+	cases := []testCase{
+		{TestName: "Empty Password", Address: "", ExpectedOutput: "Address is too short"},
+	}
+
+	for _, singleCase := range cases {
+		tempUser := User{Password: singleCase.Address}
+		err := tempUser.validatePassword()
+		if err != nil {
+			if err.Error() != singleCase.ExpectedOutput {
+				t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedOutput, err.Error())
+			}
+		} else {
+			if singleCase.ExpectedOutput != "nil" {
+				t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedOutput, "nil")
+			}
+		}
+	}
+}
