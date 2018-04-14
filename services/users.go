93a964d41a92d1ba1e60307dbd9493e449bf1c72
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-15 02:28:34 +0800

    chore: Update the user service to return the additional error parameter

--- services/users.go
+++ services/users.go
@@ -10,30 +10,30 @@ type User struct {
 	Storage
 }
 
-func (u User) GetByID(ID string) models.User {
+func (u User) GetByID(ID string) (models.User, error) {
 	var user models.User
 	// u.DB.Where("name = ?", "jinzhu").Find(&user)
 	user.FirstName = "LOL"
 	user.LastName = "MIAO"
-	return user
+	return user, nil
 }
 
-func (u User) Create(a *models.User) models.User {
+func (u User) Create(a *models.User) (models.User, error) {
 	// u.DB.Create(a)
 	lol := models.User{FirstName: "kcmkamcklamlc", LastLoginAt: time.Now()}
-	return lol
+	return lol, nil
 }
 
 type User2 struct {
 	TestData models.User
 }
 
-func (u User2) GetByID(ID string) models.User {
+func (u User2) GetByID(ID string) (models.User, error) {
 	u.TestData.FirstName = "lol"
 	u.TestData.LastName = "cacacarjnejkwkf"
-	return u.TestData
+	return u.TestData, nil
 }
 
-func (u User2) Create(a *models.User) models.User {
-	return *a
+func (u User2) Create(a *models.User) (models.User, error) {
+	return *a, nil
 }
