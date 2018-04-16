e7f823f433e3f77baf829ad0701366f5aa9efa85
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-17 01:02:06 +0800

    chore: Add more detaisl to the store

--- models/store.go
+++ models/store.go
@@ -1,8 +1,31 @@
 package models
 
+import (
+	"time"
+)
+
+// Store struct - there should only be 1 company director/admin
 type Store struct {
-	ID      string
-	Name    string
-	Address string
-	Country string
+	ID            string
+	Name          string
+	CompanyLeadID string // AdminUserID
+	Address       string
+	Country       string
+	Status        string
+	Remarks       string
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
 }
+
+func (s Store) validateName() error { return nil }
+
+func (s Store) validateCompanyLead() error { return nil }
+
+func (s Store) validateAddress() error { return nil }
+
+func (s Store) validateCountry() error { return nil }
+
+func (s Store) validateStatus() error { return nil }
+
+// Validate checks the store record to ensure data is safe to be saved
+func (s Store) Validate() error { return nil }
