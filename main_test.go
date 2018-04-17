5751abfeeca0d3e22360352452c1d7a4c9b3360c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-17 23:57:18 +0800

    chore: Altered the test accordingly to ensure that it works for the get user details section

--- main_test.go
+++ main_test.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/services"
 	"github.com/magiconair/properties/assert"
 )
 
-func TestPingRoute(t *testing.T) {
-	router := setupRouter()
+func TestUserRoutes(t *testing.T) {
+	rs := RouteServices{User: services.MockUser{}}
+	router := setupRouter(rs)
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/ping", nil)
+	req, _ := http.NewRequest("GET", "/user/12", nil)
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
+	fmt.Println(w.Body.String())
 }
