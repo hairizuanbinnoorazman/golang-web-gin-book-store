400fbf25643e4972d79ce86ad133526e8c1b9793
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-17 23:27:26 +0800

    docs: Added some comments on top of the internal validation functions.

--- models/roles.go
+++ models/roles.go
@@ -1,8 +1,13 @@
 package models
 
 import (
+	"errors"
 	"time"
 )
+
+var ErrInvalidRoleName = errors.New("Invalid Role Name")
+var ErrInvalidStatus = errors.New("Invalid Status")
+var ErrInvalidDescription = errors.New("Invalid Description")
 
 // Role struct descriptive different roles that different users can do on the platform
 type Role struct {
@@ -15,10 +20,14 @@ type Role struct {
 	UpdatedAt   time.Time
 }
 
+// There are only very few specific roles that are accepted.
+// Role Names: Managerial, StoreAdmin, Supplier
 func (r Role) validateName() error { return nil }
 
 func (r Role) validateDescription() error { return nil }
 
+// There are only very specific status that are accepted
+// Statuses: Active, Depreciated, Inactive
 func (r Role) validateStatus() error { return nil }
 
 func (r Role) validateCreatedAt() error { return nil }
