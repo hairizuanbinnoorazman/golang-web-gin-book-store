ee828b9fcfccfaa3ad172d0a418f583ca93064a0
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-19 17:58:22 +0800

    feat: Add additianal use cases for the gin book store

--- models/users_internal_test.go
+++ models/users_internal_test.go
@@ -14,6 +14,12 @@ func TestValidateUserName(t *testing.T) {
 	cases := []testCase{
 		{TestName: "Empty String", InputFirstName: "", InputLastName: "", ExpectedError: ErrNameShort},
 		{TestName: "First Name Empty", InputLastName: "acacacac", ExpectedError: ErrNameShort},
+		{TestName: "Last Name Empty", InputFirstName: "acacafacca", ExpectedError: ErrNameShort},
+		{TestName: "First Name Short", InputFirstName: "a", InputLastName: "janicanicanioanci", ExpectedError: ErrNameShort},
+		{TestName: "Last Name Short", InputLastName: "a", InputFirstName: "janicanicanioanci", ExpectedError: ErrNameShort},
+		{TestName: "First Name Long", InputLastName: "ajndjknvjksdnjkvnsjkdnvjkndfjknvjknajkvnjkvsdnjknvjknasvjkdnkjvnakjnvjknsakvnjksdvnkvksdnajkvnkjvdknavkdsnvkdsnkvnsjkdnvskjkvsdankvnkjsnakvnkvdnsjkn", InputFirstName: "janicanicanioanci", ExpectedError: ErrNameLong},
+		{TestName: "Last Name Long", InputFirstName: "ajndjknvjksdnjkvnsjkdnvjkndfjknvjknajkvnjkvsdnjknvjknasvjkdnkjvnakjnvjknsakvnjksdvnkvksdnajkvnkjvdknavkdsnvkdsnkvnsjkdnvskjkvsdankvnkjsnakvnkvdnsjkn", InputLastName: "janicanicanioanci", ExpectedError: ErrNameLong},
+		{TestName: "Last Name Long First Name Short", InputFirstName: "a", InputLastName: "ajndjknvjksdnjkvnsjkdnvjkndfjknvjknajkvnjkvsdnjknvjknasvjkdnkjvnakjnvjknsakvnjksdvnkvksdnajkvnkjvdknavkdsnvkdsnkvnsjkdnvskjkvsdankvnkjsnakvnkvdnsjkn", ExpectedError: ErrNameShort},
 	}
 
 	for _, singleCase := range cases {
@@ -37,6 +43,11 @@ func TestSetUserPassword(t *testing.T) {
 	}
 	cases := []testCase{
 		{TestName: "Empty Password", Password: "", ExpectedError: ErrPasswordShort},
+		{TestName: "Short Password", Password: "a", ExpectedError: ErrPasswordShort},
+		{TestName: "Long Password", Password: "ajsnjfnsdjknvjkdndjknjkdnjkdsnjkdsnjkfndkjfdkjsnjkfdsnjkfdsnknfsdjknfdjknjkafdsnkjdnfknfdksjnkjfnskjdnnsdkvj", ExpectedError: ErrPasswordShort},
+		{TestName: "Invalid Password Pattern", Password: "aaaaaaaa", ExpectedError: ErrPasswordInvalid},
+		{TestName: "Invalid Password Pattern with a number", Password: "aa1aaaaa", ExpectedError: ErrPasswordInvalid},
+		{TestName: "Normal Scenario", Password: "Aaaaaaa1", ExpectedError: nil},
 	}
 
 	for _, singleCase := range cases {
@@ -59,7 +70,9 @@ func TestValidateUserAddress(t *testing.T) {
 		ExpectedError error
 	}
 	cases := []testCase{
-		{TestName: "Empty Address", Address: "", ExpectedError: ErrAddressShort},
+		{TestName: "Empty Address", Address: "", ExpectedError: nil},
+		{TestName: "Long Address", Address: "ajsnjfnsdjknvjkdndjknjkdnjkdsnjkdsnjkfndkjfdkjsnjkfdsnjkfdsnknfsdjknfdjknjkafdsnkjdnfknfdksjnkjfnskjdnnsdkvj", ExpectedError: nil},
+		{TestName: "Empty Address", Address: "jkanskdkncajknjkcna cacsc cacaca cs", ExpectedError: nil},
 	}
 
 	for _, singleCase := range cases {
@@ -83,6 +96,9 @@ func TestValidateUserEmail(t *testing.T) {
 	}
 	cases := []testCase{
 		{TestName: "Empty Email", Email: "", ExpectedError: ErrEmailInvalid},
+		{TestName: "Invalid Pattern Email 1", Email: "aa@aa", ExpectedError: ErrEmailInvalid},
+		{TestName: "Invalid Pattern Email 2", Email: "@aa.ca", ExpectedError: ErrEmailInvalid},
+		{TestName: "Normal Scenario", Email: "aaaa@aa.ca", ExpectedError: ErrEmailInvalid},
 	}
 
 	for _, singleCase := range cases {
