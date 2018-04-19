4961766b071dbfb826dad1590a6fba55b62c7e5b
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-04-19 17:22:56 +0800

    chore: Altered the users mocks to return with IDs as well

--- services/users_mock.go
+++ services/users_mock.go
@@ -1,25 +1,28 @@
 package services
 
-import "github.com/hairizuanbinnoorazman/golang-web-gin-book-store/models"
+import (
+	"github.com/google/uuid"
+	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/models"
+)
 
 type MockUser struct{}
 
 func (u MockUser) GetByID(ID string) (models.User, error) {
-	a := models.User{FirstName: "John", LastName: "Kelly"}
+	a := models.User{ID: uuid.New().String(), FirstName: "John", LastName: "Kelly"}
 	return a, nil
 }
 
 func (u MockUser) GetByEmail(Email string) (models.User, error) {
-	a := models.User{FirstName: "John", LastName: "Kelly"}
+	a := models.User{ID: uuid.New().String(), FirstName: "John", LastName: "Kelly"}
 	return a, nil
 }
 
 func (u MockUser) Create(*models.User) (models.User, error) {
-	a := models.User{FirstName: "John", LastName: "Kelly"}
+	a := models.User{ID: uuid.New().String(), FirstName: "John", LastName: "Kelly"}
 	return a, nil
 }
 
 func (u MockUser) Update(*models.User) (models.User, error) {
-	a := models.User{FirstName: "John", LastName: "Kelly"}
+	a := models.User{ID: uuid.New().String(), FirstName: "John", LastName: "Kelly"}
 	return a, nil
 }
