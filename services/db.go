418d68c52a52bcb23792eac786e19dfaa0e4042c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-05 05:28:54 +0800

    feat: Remember that functions that need to edit the same object needs to have pointer access - debugging previously took a while as the function was returning new objects all the time

--- services/db.go
+++ services/db.go
@@ -1,15 +1,29 @@
 package services
 
-import "github.com/jinzhu/gorm"
+import (
+	"fmt"
+
+	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/models"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
 
 type Storage struct {
 	DB *gorm.DB
 }
 
-func (s Storage) NewDB() {
-	db, err := gorm.Open("sqlite3", "test.db")
-	if err != nil {
-		panic("failed to connect database")
+func (s *Storage) NewDB() {
+	if s.DB == nil {
+		db, err := gorm.Open("mysql", "root:@tcp(localhost:3306)/hehe?charset=utf8&parseTime=True")
+
+		if err != nil {
+			fmt.Println(err.Error())
+			panic("failed to connect database")
+		}
+		s.DB = db
 	}
-	s.DB = db
+}
+
+func (s *Storage) AutoMigrate() {
+	s.DB.AutoMigrate(&models.User{})
 }
