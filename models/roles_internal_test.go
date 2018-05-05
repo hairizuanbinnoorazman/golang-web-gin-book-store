64f6f2aab536644e7d761da7bee5a562a4e9ff41
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-06 03:13:17 +0800

    feat: Add test cases for validating names for role names and statuses

--- models/roles_internal_test.go
+++ models/roles_internal_test.go
@@ -9,17 +9,23 @@ func TestRoleValidateName(t *testing.T) {
 		ExpectedError error
 	}
 
-	cases := []testCase{}
+	cases := []testCase{
+		{TestName: "Empty Name", InputRole: Role{Name: ""}, ExpectedError: ErrInvalidRoleName},
+		{TestName: "Short Name", InputRole: Role{Name: "aa"}, ExpectedError: ErrInvalidRoleName},
+		{TestName: "Admin case", InputRole: Role{Name: "storeadmin"}, ExpectedError: nil},
+		{TestName: "Managerial case", InputRole: Role{Name: "manager"}, ExpectedError: nil},
+		{TestName: "Supplier case", InputRole: Role{Name: "supplier"}, ExpectedError: nil},
+		{TestName: "Supplier case", InputRole: Role{Name: "Admin"}, ExpectedError: ErrInvalidRoleName},
+		{TestName: "Supplier case", InputRole: Role{Name: "StoreAdmin"}, ExpectedError: ErrInvalidRoleName},
+	}
 
 	for _, singleCase := range cases {
 		err := singleCase.InputRole.validateName()
 		if err == nil && err != singleCase.ExpectedError {
 			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), "nil")
-		}
-		if singleCase.ExpectedError == nil && err != singleCase.ExpectedError {
+		} else if singleCase.ExpectedError == nil && err != singleCase.ExpectedError {
 			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, "nil", err.Error())
-		}
-		if err != singleCase.ExpectedError {
+		} else if err != singleCase.ExpectedError {
 			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), err.Error())
 		}
 	}
@@ -32,17 +38,21 @@ func TestRoleValidateStatus(t *testing.T) {
 		ExpectedError error
 	}
 
-	cases := []testCase{}
+	cases := []testCase{
+		{TestName: "Empty Status", InputRole: Role{Status: ""}, ExpectedError: ErrInvalidStatus},
+		{TestName: "Active Status", InputRole: Role{Status: "active"}, ExpectedError: nil},
+		{TestName: "Deactivated Status", InputRole: Role{Status: "deactivated"}, ExpectedError: nil},
+		{TestName: "Depreciated Status", InputRole: Role{Status: "depreciated"}, ExpectedError: nil},
+		{TestName: "Active Status", InputRole: Role{Status: "Active"}, ExpectedError: ErrInvalidStatus},
+	}
 
 	for _, singleCase := range cases {
 		err := singleCase.InputRole.validateStatus()
 		if err == nil && err != singleCase.ExpectedError {
 			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), "nil")
-		}
-		if singleCase.ExpectedError == nil && err != singleCase.ExpectedError {
+		} else if singleCase.ExpectedError == nil && err != singleCase.ExpectedError {
 			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, "nil", err.Error())
-		}
-		if err != singleCase.ExpectedError {
+		} else if err != singleCase.ExpectedError {
 			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), err.Error())
 		}
 	}
