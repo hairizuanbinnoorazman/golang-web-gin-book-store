119d3c020bdc6c82914877d6dd6780a0a8eefd6c
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-06 03:09:29 +0800

    docs: Add additional line of documentation regarding what the roles would mean and comprise of

--- models/roles.go
+++ models/roles.go
@@ -22,16 +22,20 @@ type Role struct {
 
 // There are only very few specific roles that are accepted.
 // Role Names: Managerial, StoreAdmin, Supplier
+// Role names need to be lowercased
 func (r Role) validateName() error { return nil }
 
 func (r Role) validateDescription() error { return nil }
 
 // There are only very specific status that are accepted
 // Statuses: Active, Depreciated, Inactive
+// Status of the role has to be lowercased
 func (r Role) validateStatus() error { return nil }
 
+// Need to check the value is actually created on initial save
 func (r Role) validateCreatedAt() error { return nil }
 
+// Need to check the value is actuall created on initial save and subsequent saves
 func (r Role) validateUpdatedAt() error { return nil }
 
 // Validate function runs a couple of heuristics checks to ensure that the data being passed is
