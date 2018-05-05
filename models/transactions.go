1acea9dca8595c64232d7bfbaef51bc53045e019
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-06 02:31:15 +0800

    feat: Alter the transactions to represent movement of goods/items - with receipt to be grouping of such transactions

--- models/transactions.go
+++ models/transactions.go
@@ -2,34 +2,35 @@ package models
 
 import "time"
 
-// Transaction struct describe the movement of stock in or out of a store
-// This could be movement of stock between stores or purchase of stock from supplier etc
+// Receipt is a grouping of transactions
+type Receipt struct {
+	ID          string
+	Transaction []Transaction
+}
+
+// Transaction is a a representation of movement of goods on the product
+// They can represent various types of items - e.g. Actual items, Promotional codes, Discounts etc
 type Transaction struct {
 	ID        string
+	Item      Item
+	ItemID    string
+	Qty       int
+	Price     int
+	Store     Store
 	StoreID   string
+	ReceiptID string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
 
-// Validate function checks to ensure the Transaction struct is valid
+// Validate function for the transaction item struct. This does not involve manipulating the data
+// of the transaction item.
+// We would want to ensure the follow items:
+// - Qty is not zero, it should be a 1 or -1 but never 0
+// - Price can be zero
 func (t Transaction) Validate() error { return nil }
 
-// TransactionItem is a a representation of movement of goods on the product
-// They can represent various types of items - e.g. Actual items, Promotional codes, Discounts etc
-type TransactionItem struct {
-	ID            string
-	TransactionID string
-	ItemID        string
-	Qty           int
-	Price         int
-	CreatedAt     time.Time
-	UpdatedAt     time.Time
-}
-
-func (t TransactionItem) Validate() error { return nil }
-
 type InternalTransaction struct {
-	Transaction
 	Details       string
 	RequesterID   string
 	ApproverID    string
@@ -43,7 +44,6 @@ type InternalTransaction struct {
 func (i InternalTransaction) Validate() error { return nil }
 
 type InternalTransactionItem struct {
-	TransactionItem
 	Status  string
 	Remarks string
 }
