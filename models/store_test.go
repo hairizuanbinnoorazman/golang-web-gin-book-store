c5a37b62ce498d676ece6d4abaccd8443a12f15b
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-10 00:50:15 +0800

    chore: Add tests for checking created at and updated at timings

--- models/store_test.go
+++ models/store_test.go
@@ -2,6 +2,7 @@ package models_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/hairizuanbinnoorazman/golang-web-gin-book-store/models"
 )
@@ -17,8 +18,10 @@ func TestNewStore(t *testing.T) {
 		{TestName: "Empty store", ExpectedError: models.ErrStoreNameInvalid},
 	}
 
+	timingTest := time.Now()
+
 	for _, singleCase := range cases {
-		_, err := models.NewStore()
+		store, err := models.NewStore()
 		if singleCase.ExpectedError == nil && err != nil {
 			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, "nil", err.Error())
 		} else if singleCase.ExpectedError != nil && err == nil {
@@ -28,5 +31,17 @@ func TestNewStore(t *testing.T) {
 		}
 
 		// Check for UUID being set no matter what
+		if store.ID != "" {
+			t.Errorf("Test Name: UUID check Expected Output: UUID value expected Received output: %s", store.ID)
+		}
+
+		if store.CreatedAt.After(timingTest) {
+			t.Errorf("Test Name: Created At Expected Output: Created At more than timing test Received output: %s", store.CreatedAt)
+		}
+
+		// Check for UUID being set no matter what
+		if store.UpdatedAt.After(timingTest) {
+			t.Errorf("Test Name: Updated At Expected Output: Created At more than timing tes Received output: %s", store.UpdatedAt)
+		}
 	}
 }
