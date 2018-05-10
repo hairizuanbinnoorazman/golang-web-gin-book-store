d1334762166e53cc635ffa088cf142947203ca25
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-10 23:42:11 +0800

    feat: Add tests for store struct usage

--- models/store_internal_test.go
+++ models/store_internal_test.go
@@ -47,3 +47,45 @@ func TestStoreValidateAddress(t *testing.T) {
 		}
 	}
 }
+
+func TestStoreValidateCountry(t *testing.T) {
+	type testCase struct {
+		TestName      string
+		Country       string
+		ExpectedError error
+	}
+
+	cases := []testCase{}
+
+	for _, singleCase := range cases {
+		err := Store{Country: singleCase.Country}.validateStoreCountry()
+		if err == nil && err != singleCase.ExpectedError {
+			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), "nil")
+		} else if singleCase.ExpectedError == nil && err != singleCase.ExpectedError {
+			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, "nil", err.Error())
+		} else if err != singleCase.ExpectedError {
+			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), err.Error())
+		}
+	}
+}
+
+func TestStoreValidateStatus(t *testing.T) {
+	type testCase struct {
+		TestName      string
+		Status        string
+		ExpectedError error
+	}
+
+	cases := []testCase{}
+
+	for _, singleCase := range cases {
+		err := Store{Status: singleCase.Status}.validateStoreStatus()
+		if err == nil && err != singleCase.ExpectedError {
+			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), "nil")
+		} else if singleCase.ExpectedError == nil && err != singleCase.ExpectedError {
+			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, "nil", err.Error())
+		} else if err != singleCase.ExpectedError {
+			t.Errorf("Test Name: %s Expected Output: %s Received output: %s", singleCase.TestName, singleCase.ExpectedError.Error(), err.Error())
+		}
+	}
+}
