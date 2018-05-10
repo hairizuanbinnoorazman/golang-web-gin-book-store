6c73117da952ac2a618a1604856fca1709eca18f
Hairizuan <hairizuanbinnoorazman@gmail.com>
2018-05-11 01:37:14 +0800

    feat: Implement the role status validate function. Also, fixed the status validate error name - it needs to be namespaced within the error variable to prevent confusion

--- models/roles.go
+++ models/roles.go
@@ -6,7 +6,7 @@ import (
 )
 
 var ErrInvalidRoleName = errors.New("Invalid Role Name")
-var ErrInvalidStatus = errors.New("Invalid Status")
+var ErrInvalidRoleStatus = errors.New("Invalid Status")
 var ErrInvalidDescription = errors.New("Invalid Description")
 
 // Role struct descriptive different roles that different users can do on the platform
@@ -30,12 +30,19 @@ func (r Role) validateName() error {
 	return ErrInvalidRoleName
 }
 
-func (r Role) validateDescription() error { return nil }
+func (r Role) validateDescription() error {
+	return nil
+}
 
 // There are only very specific status that are accepted
 // Statuses: Active, Depreciated, Inactive
 // Status of the role has to be lowercased
-func (r Role) validateStatus() error { return nil }
+func (r Role) validateStatus() error {
+	if r.Status == "active" || r.Status == "deactivated" || r.Status == "depreciated" {
+		return nil
+	}
+	return ErrInvalidRoleStatus
+}
 
 // Validate function runs a couple of heuristics checks to ensure that the data being passed is
 // minimally filled up
